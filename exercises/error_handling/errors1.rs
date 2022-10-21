2b239456ba58ca9f4c6b01004286fa6f5074ca2b
YuYan Li <53218750+allenli178@users.noreply.github.com>
2022-10-21 05:59:26 +0000

    🎨 complete error_handling

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -5,8 +5,6 @@
 // construct to `Option` that can be used to express error conditions. Let's use it!
 // Execute `rustlings hint errors1` for hints!
 
-// I AM NOT DONE
-
 pub fn generate_nametag_text(name: String) -> Option<String> {
     if name.len() > 0 {
         Some(format!("Hi! My name is {}", name))
@@ -24,7 +22,7 @@ mod tests {
     fn generates_nametag_text_for_a_nonempty_name() {
         assert_eq!(
             generate_nametag_text("Beyoncé".into()),
-            Ok("Hi! My name is Beyoncé".into())
+            Some("Hi! My name is Beyoncé".into())
         );
     }
 
@@ -33,7 +31,7 @@ mod tests {
         assert_eq!(
             generate_nametag_text("".into()),
             // Don't change this line
-            Err("`name` was empty; it must be nonempty.".into())
+            None
         );
     }
 }
